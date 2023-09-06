9277b32a175be5400ed28e9df32afc870c147661
sdfwefk11 <mknhsad3398@naver.com>
2023-09-06 22:11:50 +0900

    test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="text-red-500"></div>;
+  return (
+    <div className="text-red-500">
+      <h1></h1>
+    </div>
+  );
 }
