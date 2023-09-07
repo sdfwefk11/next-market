043ef4adeab55fa04fe535f7f98347991d1aa630
sdfwefk11 <mknhsad3398@naver.com>
2023-09-07 22:15:20 +0900

    Tailwind CSS2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="bg-slate-400 py-10 px-10 grid gap-5">
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
+    <div className="bg-slate-400 py-10 px-20 grid gap-5">
+      <div className="bg-white p-5 rounded-3xl shadow-xl">
         <span className="font-semibold text-2xl">Select Item</span>
         <div className="flex justify-between my-1">
           <span className="text-gray-500">Grey Chair</span>
@@ -19,9 +19,30 @@ export default function Home() {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
+        <div className="bg-blue-500 p-5 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-5 relative -top-5 bg-white">
+          <div className="flex justify-between items-end relative -top-16">
+            <div className="flex flex-col items-center -mb-2">
+              <span className="text-zinc-400 text-sm">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-lime-400 rounded-full" />
+            <div className="flex flex-col items-center -mb-2">
+              <span className="text-zinc-400 text-sm">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className="relative -mt-10 -mb-4 flex flex-col items-center ">
+            <span className="text-xl font-medium">Tony Molloy</span>
+            <span className="text-sm text-zinc-400">New York, USA</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
     </div>
   );
 }
