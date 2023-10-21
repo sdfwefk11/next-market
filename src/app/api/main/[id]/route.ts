7c943c58f47ad72668a07c81b5d6f3d183e56b4f
sdfwefk11 <mknhsad3398@naver.com>
2023-10-21 19:46:12 +0900

    Http method

---
+++ src/app/api/main/[id]/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
+
+export async function GET(req: Request) {
+  const id = req.url.slice(req.url.lastIndexOf("/") + 1);
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
+  const todo: Todo = await res.json();
+
+  if (!todo.id) return NextResponse.json({ message: "Todo not found" });
+
+  return NextResponse.json(todo);
+}
--- src/libs/client.ts
+++ src/libs/client.ts
@@ -1,16 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
 export const apiClient = new PrismaClient();
-
-async function main() {
-  await apiClient.user.create({
-    data: {
-      id: 10,
-      name: "1000점",
-    },
-  });
-}
-
-main().then(async () => {
-  await apiClient.$disconnect();
-});
