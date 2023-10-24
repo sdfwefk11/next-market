03ae052843d28e60d8db8b34fd6ec5d9b8956398
sdfwefk11 <mknhsad3398@naver.com>
2023-10-24 23:51:33 +0900

    Error message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,41 @@
 "use client";
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onVaild = () => {
-    console.log("123123");
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onVaild = (data: LoginForm) => {
+    console.log(data);
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
   };
   return (
-    <form onSubmit={handleSubmit(onVaild)}>
+    <form onSubmit={handleSubmit(onVaild, onInvalid)}>
       <input
-        {...register("username", { required: true })}
+        {...register("username", {
+          required: "username is required",
+          minLength: {
+            message: "the username should be longer than 5 chars",
+            value: 5,
+          },
+          maxLength: 20,
+        })}
         type="text"
         placeholder="Username"
       />
       <input
-        {...register("email", { required: true })}
+        {...register("email", { required: "emial is required" })}
         type="email"
         placeholder="Email"
       />
       <input
-        {...register("password", { required: true })}
+        {...register("password", { required: "password is required" })}
         type="password"
         placeholder="Password"
       />
