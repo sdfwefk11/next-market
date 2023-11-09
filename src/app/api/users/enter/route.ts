f565307481ca96bbb8a8e74cd80977cbbd3d4236
sdfwefk11 <mknhsad3398@naver.com>
2023-11-09 23:57:05 +0900

    Token

--- src/app/api/users/enter/route.ts
+++ src/app/api/users/enter/route.ts
@@ -1,28 +1,32 @@
 import { apiClient } from "@/libs/server/client";
 import { NextResponse } from "next/server";
 
+interface ResponseType {
+  ok: boolean;
+  [key: string]: any;
+}
+
 export async function POST(req: Request) {
   const { email, phone } = await req.json();
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await apiClient.user.upsert({
-    // upsert() = db에서 읽기 쓰기 수정을 간편하게 해주는 함수
-    where: {
-      ...payload,
-    }, //where = phone이 존재하면 phone을 return
-    create: {
-      name: "익명",
-      ...payload,
-      //create = phone이 존재해지 않으면 새로운 phone 데이터를 생성후 return
-    },
-    update: {},
-    //update는 하지않을 것이기 때문에 빈 obj 형태
-  });
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await apiClient.token.create({
     data: {
-      payload: "123",
-      user: { connect: { id: user.id } },
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "익명",
+            ...user,
+          },
+        },
+      },
     },
   });
+  console.log(token);
   // if (email) {
   //   user = await apiClient.user.findUnique({
   //     where: {
@@ -54,5 +58,6 @@ export async function POST(req: Request) {
   //     });
   //   }
   // }
-  return NextResponse.json(user);
+  const res: ResponseType = NextResponse.json(token);
+  return res;
 }
