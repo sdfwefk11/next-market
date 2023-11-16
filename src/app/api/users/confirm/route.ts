20f7e6d9a9805b0fa0eb51670efe180e164921fa
sdfwefk11 <mknhsad3398@naver.com>
2023-11-16 23:58:53 +0900

    Iron session

--- src/app/api/users/confirm/route.ts
+++ src/app/api/users/confirm/route.ts
@@ -1,7 +1,18 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { apiClient } from "@/libs/server/client";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request, res: NextResponse) {
   const { token } = await req.json();
+  const exists = await apiClient.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) {
+    NextResponse.error();
+  }
+
   console.log(token);
   res.status;
   return NextResponse.json(token);
