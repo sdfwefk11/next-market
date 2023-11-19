2cf5483eaf6390e621f23bf0df97bde7b8982f58
sdfwefk11 <mknhsad3398@naver.com>
2023-11-19 23:54:32 +0900

    Session

--- src/app/api/users/confirm/route.ts
+++ src/app/api/users/confirm/route.ts
@@ -1,13 +1,14 @@
-import { withIronSessionApiRoute } from "iron-session/next";
 import { NextResponse } from "next/server";
 import { NextApiRequest, NextApiResponse } from "next";
+import { cookies } from "next/headers";
+import { getIronSession } from "iron-session";
 
 export async function POST(req: Request) {
   const { token } = await req.json();
-  console.log(token);
-  withIronSessionApiRoute(token, {
+  const session = getIronSession(cookies(), {
     cookieName: "carrotsession",
-    password: "123123123131231253425",
+    password: "1231441123123123123123124425sdfad13asd1231",
   });
+  await session;
   return NextResponse.json(token);
 }
