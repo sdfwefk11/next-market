f163408b3c5046cf9303bd62d655afc1eeae8b75
sdfwefk11 <mknhsad3398@naver.com>
2023-11-21 00:27:42 +0900

    Session

--- src/app/api/users/confirm/route.ts
+++ src/app/api/users/confirm/route.ts
@@ -2,14 +2,26 @@ import { NextResponse } from "next/server";
 import { NextApiRequest, NextApiResponse } from "next";
 import { cookies } from "next/headers";
 import { getIronSession } from "iron-session";
+import apiClient from "@/libs/server/client";
 
-export async function POST(req: Request) {
+export async function POST(req: Request, res: Response) {
   const { token } = await req.json();
+  const exists = await apiClient.token.findUnique({
+    where: {
+      payload: token,
+    },
+    // include: { user: true } user에 대한 정보 또한 가져올수있다.
+  });
   console.log(token);
   const session = getIronSession(cookies(), {
     cookieName: "carrotsession",
-    password: "1231441123123123123123124425sdfad13asd1231",
+    password: process.env.CARROT_SESSION_PASSWORD!,
   });
-  session.then((res) => console.log(res));
+  console.log(exists);
+  if (!exists) res.status;
+  session.user = {
+    id: exists?.userId,
+  };
+  await (await session).save();
   return NextResponse.json(token);
 }
