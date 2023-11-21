148b8ffdf3a75f45a3695d17c02059832e9d98a3
sdfwefk11 <mknhsad3398@naver.com>
2023-11-21 23:58:12 +0900

    User

--- src/app/api/users/confirm/route.ts
+++ src/app/api/users/confirm/route.ts
@@ -4,6 +4,14 @@ import { cookies } from "next/headers";
 import { getIronSession } from "iron-session";
 import apiClient from "@/libs/server/client";
 
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
 export async function POST(req: Request, res: Response) {
   const { token } = await req.json();
   const exists = await apiClient.token.findUnique({
@@ -18,7 +26,7 @@ export async function POST(req: Request, res: Response) {
     password: process.env.CARROT_SESSION_PASSWORD!,
   });
   console.log(exists);
-  if (!exists) res.status;
+  if (!exists) return res.status;
   session.user = {
     id: exists?.userId,
   };
