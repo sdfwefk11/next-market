aafd36de0272bca775764b77d020b51a6784fc6f
sdfwefk11 <mknhsad3398@naver.com>
2023-12-06 00:37:30 +0900

    Fav api

--- src/app/api/products/[id]/fav/route.ts
+++ src/app/api/products/[id]/fav/route.ts
@@ -1,13 +1,46 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { getIronSession } from "iron-session";
-import { SessionData, sessionOption } from "@/libs/lib";
 import apiClient from "@/libs/server/client";
+import { SessionData, sessionOption } from "@/libs/lib";
 
 interface ProductId {
   params: { id: string };
 }
 
 export async function POST(req: NextRequest, { params }: ProductId) {
+  const { id } = params;
+  const session = getIronSession<SessionData>(cookies(), sessionOption);
+  const alreadyExists = await apiClient.fav.findFirst({
+    // 맨 처음에는 좋아요을 누르지 않았으니 Fav model에 데이터가 없음
+    where: {
+      productId: +id.toString(), //7
+      userId: (await session).user.id, //81
+    },
+  });
+  if (alreadyExists) {
+    //delete
+    await apiClient.fav.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    //create
+    await apiClient.fav.create({
+      data: {
+        user: {
+          connect: {
+            id: (await session).user.id,
+          },
+        },
+        product: {
+          connect: {
+            id: +id.toString(),
+          },
+        },
+      },
+    });
+  }
   return NextResponse.json({ ok: true });
 }
